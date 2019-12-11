3931dd99823b4f32baa1259093537a1815c99b69
Janssena <alexanderjanssen@live.com>
2019-12-11 16:48:28 +0100

    add binary_maps folder

--- definitions.py
+++ definitions.py
@@ -4,6 +4,7 @@ ROOT_DIR = os.path.dirname(__file__)
 
 PATH = {
     "IMAGE_IN": os.path.join(ROOT_DIR, 'data/z_stack_images/'),
+    "BINARY_MAPS": os.path.join(ROOT_DIR, 'data/binary_maps/'),
     "IMAGE_OUT": os.path.join(ROOT_DIR, 'data/z_stack_processed/'),
     "CSV": os.path.join(ROOT_DIR, 'data/z_stack_images.csv'),
 }
