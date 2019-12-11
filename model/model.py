f5bf45ee15c8b3ea64dcbb415dc4bfb4f5c455f2
Janssena <alexanderjanssen@live.com>
2019-12-11 16:46:06 +0100

    feat: encoder/decoder model

--- model/model.py
+++ model/model.py
@@ -1,12 +1,17 @@
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import ZeroPadding2D
-from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import add
 from tensorflow.keras import backend as K
+from tensorflow.keras import Sequential, models
 
 
 class VGGNet:
@@ -15,7 +20,8 @@ class VGGNet:
         # initialize the model along with the input shape to be
         # "channels last" and the channels dimension itself
         if depth > 1 or depth < 1:
-            raise ValueError('The model expects grayscale images of DAPI stained cells')
+            raise ValueError(
+                'The model expects grayscale images of DAPI stained cells')
 
         model = Sequential()
         input_shape = (height, width, depth)
@@ -76,95 +82,111 @@ class VGGNet:
         # return the constructed network architecture
         return model
 
-class CountNet:
 
-    def resnet_conv_block(input_tensor, kernel_size, filters, strides=(2,2)):
-        filter1, filter2, filter3 = filters
+def resnet_conv_block(input_tensor, kernel_size, filters, strides=(2, 2)):
+    """ Resnet conv block """
+    filter1, filter2, filter3 = filters
 
-        if K.image_data_format() == 'channels_last':
-            bn_axis = 3
-        else:
-            bn_axis = 1
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
 
-        # CONV 1
-        x = Conv2D(filter1, (1,1), strides=strides, kernel_initializer='he_normal')(input_tensor)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-        x = layers.Activation('relu')(x)
-        # CONV 2
-        x = Conv2D(filter2, kernel_size, padding='same', kernel_initializer='he_normal')(input_tensor)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-        x = layers.Activation('relu')(x)
-        # CONV 3
-        x = Conv2D(filter3, (1,1), strides=strides, kernel_initializer='he_normal')(input_tensor)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-        # SHORTCUT
-        shortcut = layers.Conv2D(filters3, (1, 1), strides=strides, kernel_initializer='he_normal')(input_tensor)
-        shortcut = layers.BatchNormalization(axis=bn_axis)(shortcut)
+    # CONV 1
+    x = Conv2D(filter1, (1, 1), strides=strides,
+               kernel_initializer='he_normal')(input_tensor)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
+    # CONV 2
+    x = Conv2D(filter2, kernel_size, padding='same',
+               kernel_initializer='he_normal')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
+    # CONV 3
+    x = Conv2D(filter3, (1, 1), kernel_initializer='he_normal')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+    # SHORTCUT
+    shortcut = Conv2D(
+        filter3, (1, 1), strides=strides, kernel_initializer='he_normal')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis)(shortcut)
 
-        x = layers.add([x, shortcut])
-        x = layers.Activation('relu')(x)
-        return x
-
-    def resnet_identity_block(input_tensor, kernel_size, filters):
-        filter1, filter2, filter3 = filters
-
-        if K.image_data_format() == 'channels_last':
-            bn_axis = 3
-        else:
-            bn_axis = 1
-
-        x = layers.Conv2D(filters1, (1, 1), kernel_initializer='he_normal')(input_tensor)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-        x = layers.Activation('relu')(x)
-
-        x = layers.Conv2D(filters2, kernel_size, padding='same', kernel_initializer='he_normal')(x)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-        x = layers.Activation('relu')(x)
-
-        x = layers.Conv2D(filters3, (1, 1), kernel_initializer='he_normal')(x)
-        x = layers.BatchNormalization(axis=bn_axis)(x)
-
-        x = layers.add([x, input_tensor])
-        x = layers.Activation('relu')(x)
-        return x
+    x = add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
 
 
-    def count_net(img_input, input_shape):
+def resnet_identity_block(input_tensor, kernel_size, filters):
+    """ Resnet identity block """
+    filter1, filter2, filter3 = filters
 
-        if K.image_data_format() == 'channels_last':
-            bn_axis = 3
-        else:
-            bn_axis = 1
-        # zero pad the image so border pixels are saved
-        x = ZeroPadding2D(padding=(3, 3))(img_input)
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
 
-        # MODEL BASE
-        x = Conv2D(16, (7, 7), strides=(2, 2), padding='valid', kernel_initializer='he_normal')(x)
-        x = BatchNormalization(axis=bn_axis)
-        x = Activation('relu')(x)
-        x = ZeroPadding2D(padding=(1, 1))(x)
-        x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = Conv2D(filter1, (1, 1),
+               kernel_initializer='he_normal')(input_tensor)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
 
-        # ENCODER
-        x = resnet_conv_block(x, (3, 3), 32)(x)
-        x = resnet_identity_block(x, (3, 3), 64)(x)
-        x = resnet_identity_block(x, (3, 3), 128)(x)
+    x = Conv2D(filter2, kernel_size, padding='same',
+               kernel_initializer='he_normal')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
 
-        # DECODER
+    x = Conv2D(filter3, (1, 1), kernel_initializer='he_normal')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+
+    x = add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
 
 
+def count_net(input_shape, input_tensor=None):
 
-    @staticmethod
-    def build(width, height, depth):
-        model = Sequential()
-        input_shape = (height, width, depth)
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
 
-        # maybe use faster R-CNN to detect object boundaries
-        # use bounding box detection algorithm to detect boxes and just count?
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    # zero pad the image so border pixels are saved
+    x = ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
 
-        # architecture:
-        #   Conv2D 16
-        #   encoder ResNet50 block -> ConvBlock 32 - Identity 64 - Identity 128
-        #   decoder ResNet50 block -> Identity 64 - Identity 32 - ConvBlock 16
-        #   GlobalAvgPool
-        #   Dense
+    # MODEL BASE
+    x = Conv2D(16, (7, 7), strides=(2, 2), padding='valid',
+               kernel_initializer='he_normal', input_shape=input_shape, name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
+
+    # ENCODER
+    x = resnet_conv_block(x, (3, 3), (32, 32, 4*32))
+    x = resnet_identity_block(x, (3, 3), (32, 32, 4*32))
+    x = resnet_conv_block(x, (3, 3), (64, 64, 4*64))
+    x = resnet_identity_block(x, (3, 3), (64, 64, 4*64))
+    x = resnet_identity_block(x, (3, 3), (64, 64, 4*64))
+    x = resnet_conv_block(x, (3, 3), (128, 128, 4*128))
+    x = resnet_identity_block(x, (3, 3), (128, 128, 4*128))
+    x = resnet_identity_block(x, (3, 3), (128, 128, 4*128))
+
+    # DECODER
+    x = resnet_identity_block(x, (3, 3), (128, 128, 4*128))
+    x = resnet_identity_block(x, (3, 3), (128, 128, 4*128))
+    x = resnet_conv_block(x, (3, 3), (128, 128, 4*128), strides=(1, 1))
+    x = Conv2DTranspose(128, (3, 3), strides=(2, 2))(x)
+    x = resnet_identity_block(x, (3, 3), (64, 64, 2*64))
+    x = resnet_conv_block(x, (3, 3), (64, 64, 4*64), strides=(1, 1))
+    x = Conv2DTranspose(64, (3, 3), strides=(2, 2))(x)
+    x = resnet_identity_block(x, (3, 3), (32, 32, 2*32))
+    x = resnet_conv_block(x, (3, 3), (32, 32, 4*32), strides=(1, 1))
+    x = Conv2DTranspose(32, (3, 3), strides=(2, 2))(x)
+
+    # TAIL
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+    inputs = img_input
+    model = models.Model(inputs, x, name='countnet')
+    return model
