db62d956607386771413f6179ea21c3a603bf0c2
Janssena <alexanderjanssen@live.com>
2019-12-23 10:02:08 +0100

    output folder for trained weights

--- definitions.py
+++ definitions.py
@@ -9,4 +9,5 @@ PATH = {
     "SIMCEP_CSV": os.path.join(ROOT_DIR, 'data/simcep/simcep.csv'),
     "IMAGE_OUT": os.path.join(ROOT_DIR, 'data/z_stack_processed/'),
     "CSV": os.path.join(ROOT_DIR, 'data/z_stack_images.csv'),
+    "MODEL_OUT": os.path.join(ROOT_DIR, 'model/out')
 }
